8622b214eac8df5e802e2c03620fc086a2dcf716
lizy <zhenyu_li1998@163.com>
2026-07-25 21:57:12 +0800

    fix(packages): resolve test vs flutter_test dependency conflict in workspace
    - Remove test: ^1.25.0 from gcode_core dev_dependencies (unused)
    - Remove test: ^1.25.0 from flutter_ioc_core dev_dependencies (unused)
    - Switch gcode_core tests from package:test to package:flutter_test
    - Workspace resolution now succeeds with flutterguard_cli present

--- packages/gcode_core/test/application/gcode_readline_pipeline_test.dart
+++ packages/gcode_core/test/application/gcode_readline_pipeline_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gcode_core/gcode_core.dart';
 
 void main() {
--- packages/gcode_core/test/gcode_parser_test.dart
+++ packages/gcode_core/test/gcode_parser_test.dart
@@ -1,4 +1,4 @@
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gcode_core/gcode_core.dart';
 
 void main() {
